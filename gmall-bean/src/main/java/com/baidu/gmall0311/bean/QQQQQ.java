620758be6aeb9e9817bb7717b398d3385e570fc4
Alei <541631380@qq.com>
2019-08-25 09:41:04 +0800

    qqq

---
+++ gmall-bean/src/main/java/com/baidu/gmall0311/bean/QQQQQ.java
@@ -0,0 +1,12 @@
+package com.baidu.gmall0311.bean;
+
+/**
+ * @author Alei
+ * @create 2019-08-25 9:40
+ */
+public class QQQQQ {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+
+}
